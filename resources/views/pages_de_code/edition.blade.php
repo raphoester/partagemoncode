14896d8f620ac753c3c00f791802df2b6c65d48c
raphoester <raphael.oester@ynov.com>
2021-01-28 09:52:37 +0100

    deb edition code

---
+++ resources/views/pages_de_code/edition.blade.php
@@ -0,0 +1,11 @@
+@extends('app.layout')
+
+@section('content')
+<div class="container">
+    <form action="" method='POST'>
+        <input type="textarea">
+        <input type="submit">
+
+    </form>
+</div>
+@endsection
