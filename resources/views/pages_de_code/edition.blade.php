fa611af12dfdac718ee198fe2227955f5755b456
raphoester <raphael.oester@ynov.com>
2021-01-28 12:58:52 +0100

    petits détails d'affichage

--- resources/views/pages_de_code/edition.blade.php
+++ resources/views/pages_de_code/edition.blade.php
@@ -2,7 +2,13 @@
 
 @section('contenu')
 <div class="container">
-    
+    <h1>{{$code->titre}}</h1>
+    @if($code->prive == 0)
+    @php
+        $url = env('APP_URL').'/'.$code->id;
+    @endphp
+    <h5>URL d'accès publique : <a href="{{$url}}">{{$url}}</a></h5>
+    @endif
     <form action="" method='POST'>
         @csrf
         <textarea name="contenu" id="" cols="30" rows="10" style="width: 100%; height: 100vh;">{{$code->contenu}}</textarea>
--- resources/views/pages_de_code/liste_pages.blade.php
+++ resources/views/pages_de_code/liste_pages.blade.php
@@ -2,11 +2,14 @@
 
 @section('contenu')
 <div class="container">
-    <div><h1>Liste de vos pages de code</h1></div>
+    <div>
+        <h1>Liste de vos pages de code</h1>
+    </div>
     <a href="/creerPage">Nouvelle page de code</a>
     @foreach($pages as $page)
     <div>
-        <h3><a href="/edition/{{$page->id}}">{{$page->titre}}</a></h3>
+        <h3><a href="/edition/{{$page->id}}">{{$page->titre}} @if($page->prive)🔒@endif</a></h3>
+
     </div>
     @endforeach
 </div>
