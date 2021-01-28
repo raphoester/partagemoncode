432f413b2c352d45b9bc3f30cb174a676f5382da
raphoester <raphael.oester@ynov.com>
2021-01-28 09:29:32 +0100

    traduction des auth en francais

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Vérifiez votre courriel') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Un lien de vérification vous a été envoyé par mail.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Avant de continuer, cliquez sur le lien de vérification que nous vous avons envoyé par mail.') }}
+                    {{ __('Si vous n'avez pas reçu de mail') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('cliquez ici pour en recevoir un autre.') }}</button>.
                     </form>
                 </div>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts/app')
 @section('contenu')
 
+<div class='container'>
+    <div>
+        <h1>PartageMonCode est un simple outil de collaboration entre développeurs.</h1>
+    </div>
+    <div>
+        <p>
+            Entrez du code, partagez l'url et voyez la magie s'opérer :)
+        </p>
+    </div>
 
+
+
+</div>
 
 @endsection
